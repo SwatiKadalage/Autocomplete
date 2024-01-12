40b2c2657bafebd752719d8e5f10307deaff7f0e
sejal <sejalkulkarni2004@gmail.com>
2024-01-12 15:05:28 +0530

    Autocomplete

---
+++ app/src/main/java/com/example/autocomplete/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.autocomplete;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] country={"India","USA","Africa","UK","Australia","France","Ingland"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,country);
+        AutoCompleteTextView auto_view=(AutoCompleteTextView) findViewById(R.id.auto);
+        auto_view.setThreshold(1);
+        auto_view.setAdapter(adapter);
+        auto_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Selected Item="+parent.getSelectedItem(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
